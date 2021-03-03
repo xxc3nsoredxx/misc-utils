cf6d2ede9b8f2a12754627248c3482377d73754d
xxc3nsoredxx <xxc3ncoredxx@gmail.com>
2021-03-03 15:42:49 -0600

    Outlook script fix type
    Typo in redirect URL

--- userscripts/outlook_logout_redirect.user.js
+++ userscripts/outlook_logout_redirect.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Outlook Logout Redirect
-// @version     1
+// @version     1.1
 // @description Redirect to login page after logout message appears
 // @match       *://login.microsoftonline.com/common/oauth2/logout
 // @run-at      document-idle
@@ -8,7 +8,7 @@
 
 const id = 'login_workload_logo_text';
 const test_phrase = 'signed out of your account';
-const redirect = 'https://outlook.offile.com';
+const redirect = 'https://outlook.office.com';
 
 if (document.getElementById(id).innerText.includes(test_phrase)) {
     window.location.replace(redirect);
