4d6a828496a22a3c466d3c0a9ff79cc4ef325ad8
xxc3nsoredxx <xxc3ncoredxx@gmail.com>
2021-03-05 15:14:28 -0600

    Update Outlook script
    Uses a timing event based method because sometimes the target string
    isn't on the page right away (ie, there's intermediate content). Begins
    running the script as soon as the page has loaded instead of waiting for
    everything to finish.

--- userscripts/outlook_logout_redirect.user.js
+++ userscripts/outlook_logout_redirect.user.js
@@ -1,16 +1,19 @@
 // ==UserScript==
 // @name        Outlook Logout Redirect
 // @namespace   https://github.com/xxc3nsoredxx/misc-utils/
-// @version     1.2
+// @version     2.0
 // @description Redirect to login page after logout message appears
 // @match       *://login.microsoftonline.com/common/oauth2/logout
-// @run-at      document-idle
+// @run-at      document-end
 // ==/UserScript==
 
-const id = 'login_workload_logo_text';
-const test_phrase = 'signed out of your account';
 const redirect = 'https://outlook.office.com';
+const test_phrase = 'signed out of your account';
 
-if (document.getElementById(id).innerText.includes(test_phrase)) {
-    window.location.replace(redirect);
+function check_page () {
+    if (document.body.innerText.includes(test_phrase)) {
+        window.location.replace(redirect);
+    }
 }
+
+let timer = setInterval(check_page, 1000);
