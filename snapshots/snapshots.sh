0c153445dd97c324ec3f7d616c78c721ca19cf11
xxc3nsoredxx <xxc3ncoredxx@gmail.com>
2021-05-16 16:40:48 -0500

    Add the code for transfer mode

--- snapshots/snapshots.sh
+++ snapshots/snapshots.sh
@@ -16,9 +16,7 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# TODO: flag for taking snapshots
-# TODO: pretend mode for taking snapshots
-# TODO: flag for transfering snapshots
+# TODO: manually take a set of snapshots, ignoring date limits
 
 
 # Ensure the return value of a pipeline is 0 only if all commands in it succeed
@@ -82,11 +80,10 @@ EXPIRED="$(date -I --date="$TAKE_EXPIRED_CUTOFF")"
 # Oldest snapshot to keep on source
 OLDEST=$(date -I --date="$XFER_OLDEST_CUTOFF")
 # udev creates a symlink /dev/ss_crypt pointing to the partition
-LUKS_DEVICE='/dev/ss_crypt'
 LUKS_NAME='ss_crypt'
+LUKS_DEVICE="/dev/$LUKS_NAME"
 # Flags to control what is cleaned up
 LUKS_OPENED=0
-LUKS_MOUNTED=0
 # cryptsetup(8) exit codes
 CRYPT_SUCCESS=0
 CRYPT_EXISTS_BUSY=5
@@ -191,33 +188,60 @@ try_unmount () {
     fi
 }
 
-# Only close/unmount any drives opened/mounted by the script
-cleanup () {
-    try_unmount 'src'
-    try_unmount 'dest'
+# Try to open a LUKS crypt
+# Arg 1: LUKS device name
+# Arg 2: path to LUKS keyfile
+try_open () {
+    # Check that udev's symlink exists
+    if [ ! -e "/dev/$1" ]; then
+        die "/dev/$1 not found"
+    fi
 
+    # Open LUKS crypt
+    cryptsetup --key-file "$2" open --type luks "/dev/$1" "$1"
+    STATUS=$?
+    if [ $STATUS -eq $CRYPT_SUCCESS ]; then
+        log "Opened /dev/$1 as $1"
+        LUKS_OPENED=1
+    elif [ $STATUS -eq $CRYPT_EXISTS_BUSY ]; then
+        log "$1 already opened"
+    else
+        die "Failed to open /dev/$1 as $1"
+    fi
+}
+
+# Try to close a LUKS crypt
+# Arg 1: LUKS device name
+try_close () {
     if [ $LUKS_OPENED -eq 1 ]; then
         # Attempt to close LUKS device. If busy, wait a bit and retry a few
         # times. Error if all unsuccessful.
         for i in {1..3}; do
-            log "Attempt $i to close $LUKS_NAME"
-            cryptsetup close $LUKS_NAME
+            log "Attempt $i to close $1"
+            cryptsetup close "$1"
             STATUS=$?
 
             if [ $STATUS -eq $CRYPT_EXISTS_BUSY ]; then
                 sleep 0.1
             elif [ $STATUS -eq $CRYPT_SUCCESS ]; then
-                log "Closed $LUKS_NAME"
+                log "Closed $1"
                 break
             else
-                die "Failed to close $LUKS_NAME"
+                die "Failed to close $1"
             fi
         done
 
         if [ $STATUS -ne $CRYPT_SUCCESS ]; then
-            die "Failed to close $LUKS_NAME"
+            die "Failed to close $1"
         fi
     fi
+}
+
+# Only close/unmount any drives opened/mounted by the script
+cleanup () {
+    try_unmount 'src'
+    try_unmount 'dest'
+    try_close "$LUKS_NAME"
 }
 
 # Try to take a snapshot
@@ -258,7 +282,7 @@ try_transfer () {
         fi
     # Mode 2, with base
     elif [ $# -eq 4 ]; then
-        log ">>> Sending $1/$2 using $3 as the base"
+        log ">>> Sending $2 using $3 as the base"
         if [ $PRETEND -eq 0 ]; then
             if ! (btrfs send -p "$1/$3" "$1/$2" | btrfs receive "$4"); then
                 die "Error sending subvolume $1/$2, base $1/$3"
@@ -334,8 +358,15 @@ fi
 
 # Set up for transfering snapshots
 if [ $XFER_MODE -eq 1 ]; then
-    echo 'Transfer mode not implemented yet!'
-    exit 1
+    SRC_SNAPSHOTS_DEV="$XFER_SRC_SNAPSHOTS_DEV"
+    SRC_SNAPSHOTS="$XFER_SRC_SNAPSHOTS"
+    SRC_MOUNT_OPTS=("${XFER_SRC_MOUNT_OPTS[@]}")
+
+    DEST_SNAPSHOTS_DEV="/dev/mapper/$LUKS_NAME"
+    DEST_SNAPSHOTS="$XFER_DEST_SNAPSHOTS"
+    DEST_MOUNT_OPTS=("${XFER_DEST_MOUNT_OPTS[@]}")
+
+    declare -n SUBVOLUMES=XFER_SUBVOLUMES
 fi
 
 # Create file descriptors for syslog info and syslog err
@@ -350,6 +381,13 @@ if [ "$(id -u)" -ne 0 ]; then
     die "Snapshots can only be managed by root ($(id -un), id=$(id -u))"
 fi
 
+# Try to mount the snapshot source and destination
+try_mount 'src'
+if [ $XFER_MODE -eq 1 ]; then
+    try_open "$LUKS_NAME" "$XFER_LUKS_KEYFILE"
+fi
+try_mount 'dest'
+
 # Check that valid dates were acquired
 if [ $TAKE_MODE -eq 1 ]; then
     if [ -z "$DESIRED_PREV" ]; then
@@ -358,13 +396,17 @@ if [ $TAKE_MODE -eq 1 ]; then
     if [ -z "$EXPIRED" ]; then
         die "Invalid date string '$EXPIRED_CUTOFF'"
     fi
+
+    log "Desired previous: $DESIRED_PREV"
+    log "Expired: $EXPIRED"
 fi
+if [ $XFER_MODE -eq 1 ]; then
+    if [ -z "$OLDEST" ]; then
+        die "Invalid date string '$XFER_OLDEST_CUTOFF'"
+    fi
 
-# Try to mount the snapshot source and destination
-try_mount 'src'
-try_mount 'dest'
-
-exit
+    log "Oldest: $OLDEST"
+fi
 
 # Loop through the keys of the SUBVOLUMES map
 for target in "${!SUBVOLUMES[@]}"; do
@@ -377,52 +419,89 @@ for target in "${!SUBVOLUMES[@]}"; do
     mapfile -t list_src < <(ls "$target_src")
     mapfile -t list_dest < <(ls "$target_dest")
 
-    # If no snapshots exist, make one
-    if [ ${#list_src[@]} -eq 0 ]; then
-        log "No existing snapshots found on $SRC_SNAPSHOTS"
+    # Snapshot mode
+    if [ $TAKE_MODE -eq 1 ]; then
+        # If no snapshots exist, make one
+        if [ ${#list_src[@]} -eq 0 ]; then
+            log "No existing snapshots found on $SRC_SNAPSHOTS"
 
-        try_snapshot "$target" "$target_src" "$NAME"
-        try_transfer "$target_src" "$NAME" "$target_dest"
+            try_snapshot "$target" "$target_src" "$NAME"
+            try_transfer "$target_src" "$NAME" "$target_dest"
 
-        continue
-    fi
-
-    # Check if most recent src snapshot is up to date
-    src_prev="${list_src[-1]}"
-    if [[ "$src_prev" < "$DESIRED_PREV" ]]; then
-        log "SRC snapshot '$target_src/$src_prev' is out of date"
-
-        try_snapshot "$target" "$target_src" "$NAME"
-
-        # Add to the end of src snapshots list and set as previous snapshot
-        list_src=("${list_src[@]}" "$NAME")
-        src_prev=$NAME
-    fi
-
-    # Check if most recent dest snapshot is up to date
-    dest_prev="${list_dest[-1]}"
-    if [[ "$dest_prev" < "$src_prev" ]]; then
-        log "DEST snapshot '$target_dest/$dest_prev' is out of date"
-
-        # Determine if incremental send is used
-        if [ ${#list_src[@]} -ge 2 ]; then
-            try_transfer "$target_src" "$src_prev" "${list_src[-2]}" "$target_dest"
-        else
-            try_transfer "$target_src" "$src_prev" "$target_dest"
+            continue
         fi
-    fi
 
-    # Check for expired src snapshots
-    # Only delete if there are > 2 snapshots so that incremental send works
-    if [ ${#list_src[@]} -gt 2 ]; then
-        for del in "${list_src[@]}"; do
-            if [[ "$del" < "$EXPIRED" ]]; then
-                log "Expired snapshot '$target_src/$del'"
-                try_delete "$target_src" "$del"
+        # Check if most recent src snapshot is up to date
+        src_prev="${list_src[-1]}"
+        log "Latest src: $src_prev"
+        if [[ "$src_prev" < "$DESIRED_PREV" ]]; then
+            try_snapshot "$target" "$target_src" "$NAME"
+
+            # Add to the end of src snapshots list and set as previous snapshot
+            list_src=("${list_src[@]}" "$NAME")
+            src_prev=$NAME
+        fi
+
+        # Check if most recent dest snapshot is up to date
+        dest_prev="${list_dest[-1]}"
+        log "Latest dest: $dest_prev"
+        if [[ "$dest_prev" < "$src_prev" ]]; then
+            # Determine if incremental send is used
+            if [ ${#list_src[@]} -ge 2 ]; then
+                try_transfer "$target_src" "$src_prev" "${list_src[-2]}" "$target_dest"
+            else
+                try_transfer "$target_src" "$src_prev" "$target_dest"
+            fi
+        fi
+
+        # Check for expired src snapshots
+        # Only delete if there are > 2 snapshots so that incremental send works
+        if [ ${#list_src[@]} -gt 2 ]; then
+            for del in "${list_src[@]}"; do
+                if [[ "$del" < "$EXPIRED" ]]; then
+                    log "Expired snapshot '$target_src/$del'"
+                    try_delete "$target_src" "$del"
+                fi
+            done
+        else
+            log "Not enough snapshots in '$target_src' to expire"
+        fi
+    # Transfer mode
+    elif [ $XFER_MODE -eq 1 ]; then
+        # Get the most recent snapshot on the destination drive to use as a base
+        base=''
+        if [ ${#list_dest} -eq 0 ]; then
+            log "No '$target' snapshots on $LUKS_NAME"
+        else
+            base=${list_dest[-1]}
+            log "Most recent '$target' snapshot on $LUKS_NAME: $base"
+        fi
+
+        # Transfer snapshots
+        for ss in "${list_src[@]}"; do
+            if [[ "$ss" < "$OLDEST" ]]; then
+                # Handle newest on destination == oldest on source
+                # Used to set the base when not starting from a blank slate
+                if [ "$ss" == "$base" ]; then
+                    log "$ss already exists on $LUKS_NAME, skipped"
+                # Handle no previous snapshot case
+                # Used when first sending snapshots to a new drive
+                elif [ -z "$base" ]; then
+                    try_transfer "$target_src" "$ss" "$target_dest"
+                # Normal case
+                # Newest on destination is used as the base, sends second oldest on source
+                else
+                    try_transfer "$target_src" "$ss" "$base" "$target_dest"
+
+                    # Deleting only the base snapshot preserves the most recent
+                    # moved snapshot to use as a base in the future
+                    try_delete "$target_src" "$base"
+                fi
+
+                # Update base
+                base=$ss
             fi
         done
-    else
-        log "Not enough snapshots in '$target_src' to expire"
     fi
 done
 
