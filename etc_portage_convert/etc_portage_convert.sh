ded3d02997b3369a404b23db9878f7ec4a866b0c
Oskari Pirhonen <xxc3ncoredxx@gmail.com>
2022-10-03 01:03:29 -0500

    Use the cool Gentoo functions for messages
    Fall back to boring messages if /lib/gentoo/functions.sh is not found.

--- etc_portage_convert/etc_portage_convert.sh
+++ etc_portage_convert/etc_portage_convert.sh
@@ -17,6 +17,10 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 DEBUG=${DEBUG@L}
+GENTOO_FUNCTIONS="/lib/gentoo/functions.sh"
+USE_GENTOO=0
+# Don't log to syslog with the Gentoo functions
+EINFO_LOG=""
 
 # Print message to stderr
 err () {
@@ -25,8 +29,8 @@ err () {
 
 # Print message to stderr only if debug is enabled
 debug () {
-    if [ $# -gt 0 ]; then
-        if [ "${DEBUG:0:1}" == y ] || [ "${DEBUG:0:1}" == 1 ]; then
+    if [ "${DEBUG:0:1}" == y ] || [ "${DEBUG:0:1}" == 1 ]; then
+        if [ $# -gt 0 ]; then
             err "DDD $@"
         fi
     fi
@@ -34,7 +38,11 @@ debug () {
 
 # Print error to stderr
 error () {
-    [ $# -gt 0 ] && err "!!! $@"
+    if [ $USE_GENTOO -eq 1 ]; then
+        [ $# -gt 0 ] && eerror "$@"
+    else
+        [ $# -gt 0 ] && err "!!! $@"
+    fi
 }
 
 # Print message to stderr (if given) and exit with failure
@@ -43,9 +51,25 @@ die () {
     exit 1
 }
 
+# Print an informative message
+info () {
+    if [ $USE_GENTOO -eq 1 ]; then
+        [ $# -gt 0 ] && einfo "$@"
+    else
+        echo "$@"
+    fi
+}
+
+
 debug "debug on"
 
 # Basic sanity checks
+if [ ! -f "$GENTOO_FUNCTIONS" ]; then
+    error "$GENTOO_FUNCTIONS not found, using boring messages ;("
+else
+    source "$GENTOO_FUNCTIONS"
+    USE_GENTOO=1
+fi
 [ $(id -u) -eq 0 ]  || die "$0 must be run as root"
 PORTAGEQ="$(command -v portageq)"
 [ -n "$PORTAGEQ" ]  || die "portageq not found"
@@ -59,12 +83,9 @@ if [ $? -ne 0 ]; then
     rm -rf "$BU_DIR"
     die "unable to backup $CONF_DIR to $BU_DIR"
 fi
-echo "backup: $BU_DIR"
+info "backup: $BU_DIR"
 
 # Check for flat files that can be turned into directories
-# TODO:
-#   - include make.conf in this or no? idk if jannik was being silly or serious
-#     (my guess is silly)
 for file in "$CONF_DIR/repos.conf" \
             "$CONF_DIR"/package.*
 do
@@ -74,7 +95,7 @@ do
 
     # If it's already a directory, safe to skip
     if [ -d "$file" ]; then
-        echo "skip $file: already a directory"
+        info "skip $file: already a directory"
         continue
     fi
 
@@ -82,27 +103,45 @@ do
     temp="$(mktemp "$file.XXXXXX")"
     debug "using temp file $temp"
 
+    [ $USE_GENTOO -eq 1 ] && ebegin "convert: $file -> $file/$new_file"
+
     mv "$file" "$temp"
     if [ $? -ne 0 ]; then
-        error "unable to back up $file to temp file, skipping..."
+        if [ $USE_GENTOO -eq 1 ]; then
+            ewend 1 "unable to back up to temp file, skipping..."
+        else
+            error "unable to back up $file to temp file, skipping..."
+        fi
         rm -f "$temp"
         continue
     fi
 
     mkdir "$file"
     if [ $? -ne 0 ]; then
-        error "unable to create directory for $file, restoring and skipping..."
+        if [ $USE_GENTOO -eq 1 ]; then
+            ewend 1 "unable to create directory, restoring and skipping..."
+        else
+            error "unable to create directory for $file, restoring and skipping..."
+        fi
         mv "$temp" "$file"
         continue
     fi
 
     mv "$temp" "$file/$new_file"
     if [ $? -ne 0 ]; then
-        error "unable to move backup to directory $file, restoring and skipping..."
+        if [ $USE_GENTOO -eq 1 ]; then
+            ewend 1 "unable to move backup to directory, restoring and skipping..."
+        else
+            error "unable to move backup to directory $file, restoring and skipping..."
+        fi
         rm -rf "$file"
         mv "$temp" "$file"
         continue
     fi
 
-    echo "convert: $file -> $file/$new_file"
+    if [ $USE_GENTOO -eq 1 ]; then
+        eend
+    else
+        info "convert: $file -> $file/$new_file"
+    fi
 done
